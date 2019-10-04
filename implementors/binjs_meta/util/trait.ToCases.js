a32dfdb0f2303f9733de8dbdff18e51288c8705a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-04 13:09:47 +0000

    Deploy binast/binjs-ref to github.com/binast/binjs-ref.git:gh-pages

---
+++ implementors/binjs_meta/util/trait.ToCases.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["binjs_meta"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/brotli_decompressor/io_wrappers/trait.CustomRead.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["brotli_decompressor"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
